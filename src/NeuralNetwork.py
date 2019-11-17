15e97a360f8bd4ff3085569d5b9a5a2bca543bde
Gabriel Tadiello <gtmoraes@inf.ufrgs.br>
2019-11-17 19:33:03 -0200

    Working as expected on numerical check example (moodle)

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -47,11 +47,11 @@ class NeuralNetwork():
             self.gradient.append(np.empty((self.architecture[i+1],
                                           self.architecture[i]+1)))
 
-        self.architecture = np.asarray(self.architecture)
-        self.activation = np.asarray(self.activation)
-        self.weights = np.asarray(self.weights)
-        self.delta = np.asarray(self.delta)
-        self.gradient = np.asarray(self.gradient)
+        # self.architecture = np.asarray(self.architecture)
+        # self.activation = np.asarray(self.activation)
+        # self.weights = np.asarray(self.weights)
+        # self.delta = np.asarray(self.delta)
+        # self.gradient = np.asarray(self.gradient)
 
 
     def readNetworkWeights(self, fileName):
@@ -77,13 +77,16 @@ class NeuralNetwork():
                 self.weights[i][j] = actualWeights
 
     # Single element
-    def computeActivations(self, input):
+    def computeActivations(self, input, eps=None):
         assert len(input) == len(self.activation[0])-1, \
             'Error: input size is not compatible with network shape'
 
         self.activation[0][1:] = input
         for i in range(1, len(self.activation)):
-            self.activation[i][1:] = (self.activation[i-1] * self.weights[i-1]).sum(1)
+            if eps is None:
+                self.activation[i][1:] = (self.activation[i-1] * self.weights[i-1]).sum(1)
+            else:
+                self.activation[i][1:] = (self.activation[i-1] * (self.weights[i-1] + eps[i-1])).sum(1)
             self.activation[i][1:] = sigmoid(self.activation[i][1:])
 
     # Single element
@@ -94,9 +97,9 @@ class NeuralNetwork():
         # Reverse loop, from last hidden layer to the first one
         for i in range(len(self.delta)-2, -1, -1):
             # Skips bias activation
-            self.delta[i] = (self.delta[i+1] * self.weights[i+1][:,1:]) * \
+            self.delta[i] = ((self.delta[i+1] * self.weights[i+1][:,1:]) * \
                             self.activation[i+1][1:] * \
-                            (np.ones(len(self.activation[i+1][1:])) - self.activation[i+1][1:])
+                            (np.ones(len(self.activation[i+1][1:])) - self.activation[i+1][1:]))[0]
 
     def setGradientZero(self):
         for g in self.gradient:
@@ -105,6 +108,14 @@ class NeuralNetwork():
     # Single element
     def updateGradient(self, x, y):
         self.computeDeltas(x, y)
+
+        if self.verbose:
+            print('Weights:')
+            print(self.weights)
+            print('Activation:')
+            print(self.activation)
+            print('Delta:')
+            print(self.delta)
 
         for i in range(len(self.gradient)):
             # Delta already starts in the first hidden layer
@@ -117,6 +128,9 @@ class NeuralNetwork():
         for layer in regularization:
             layer[:,0] = 0
 
+        if self.verbose:
+            print('Gradient:')
+            print(self.gradient)
 
         self.gradient = (1.0 / n) * (self.gradient + regularization)
 
@@ -131,12 +145,12 @@ class NeuralNetwork():
             self.weights -= self.alpha * self.gradient
 
     # Batch
-    def computeCost(self, x, y):
+    def computeCost(self, x, y, eps=None, reg=True):
         assert len(x) == len(y), "Error: number of entries and labels don't match"
 
         j = 0.0
         for i in range(len(x)):
-            self.computeActivations(x[i])
+            self.computeActivations(x[i], eps)
             out = self.getPredictions();
 
             j += ([-1*v for v in y[i]] * np.log(out) - (np.ones(len(y[i])) - y[i]) * np.log(1 - out)).sum()
@@ -144,9 +158,11 @@ class NeuralNetwork():
 
 
         s = 0.0
-        for w in self.weights:
-            s += np.square(w[1:]).sum()
-        s *= (self.regularizationFactor / (2*len(x)))
+
+        if reg:
+            for w in self.weights:
+                s += np.square(w[1:]).sum()
+            s *= (self.regularizationFactor / (2*len(x)))
 
         if self.verbose:
             print('J: ', j)
@@ -156,6 +172,39 @@ class NeuralNetwork():
 
     def getPredictions(self):
         return self.activation[len(self.activation)-1][1:]
+
+    def numericalCheck(self, x, y):
+        eps = 0.000005
+        diff = [np.zeros_like(arr) for arr in self.gradient]
+        nGradient = [np.zeros_like(arr) for arr in self.gradient]
+        # print(self.gradient[0].shape)
+        # print(self.gradient[1].shape)
+
+        self.setGradientZero()
+        for j in range(len(x)):
+            self.updateGradient(x[j], y[j])
+        print('Computed gradient:')
+        print(self.gradient)
+
+        for l in range(len(self.gradient)):
+            for i in range(self.architecture[l+1]):
+                for j in range(self.architecture[l]+1):
+                    diff[l][i][j] = eps
+                    # print('DIFF:', diff[i][0][j])
+                    # print(diff)
+                    J = self.computeCost(x, y, diff, False)
+                    diff[l][i][j] = -eps
+                    # print('DIFF:')
+                    # print(diff)
+                    J -= self.computeCost(x, y, diff, False)
+                    diff[l][i][j] = 0.0
+                    # print(i, j)
+                    nGradient[l][i][j] = J / (2 * eps)
+
+        print('DIFF:')
+        print(diff)
+        print('Numerical gradient:')
+        print(nGradient)
 
     def predict(self, x):
         self.computeActivations(x)
@@ -184,10 +233,14 @@ class NeuralNetwork():
             self.updateTheta()
 
 if __name__ == "__main__":
-    a = NeuralNetwork(0.1)
-    a.readNetworkArchitecture("testfiles/network.txt")
-#    a.readNetworkWeights("testfiles/weights.txt")
+#    x = [[0.6969, 0.666]]; y = [[1.0]]
+    x = [[1.5]]; y = [[1.0]]
 
-    print('Before training....', a.predict([0.6969, 0.666]), '\n')
-    a.train([[0.6969, 0.666]], [[1.0]], 5)
-    print('\n\nAfter training....', a.predict([0.6969, 0.666]))
+    a = NeuralNetwork(0.1)
+    a.readNetworkArchitecture("testfiles/network_numerical_test.txt")
+    a.readNetworkWeights("testfiles/weights_numerical_test.txt")
+    a.numericalCheck(x, y)
+
+#    print('Before training....', a.predict([0.6969, 0.666]), '\n')
+#    a.train(x, y, 1)
+#    print('\n\nAfter training....', a.predict([0.6969, 0.666]))
